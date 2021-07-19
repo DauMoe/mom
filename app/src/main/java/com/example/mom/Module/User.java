501b6aa9283d2fce21cbff9bf9dd319577f50dbb
DauMoe <lehuuyhoang1111999@gmail.com>
2021-07-19 16:35:40 +0700

    (DONE) Layout update info

--- app/src/main/java/com/example/mom/Module/User.java
+++ app/src/main/java/com/example/mom/Module/User.java
@@ -1,13 +1,13 @@
 package com.example.mom.Module;
 
 public class User {
-    private String username, email, phone, unit, uniqueID, GroupID, PIN;
+    private String username, email, phone, unit, uniqueID, GroupID, PIN, address, sex, dob;
     private long amount;
 
     public User() {
     }
 
-    public User(String username, String email, String phone, String unit, String uniqueID, String groupID, String PIN, long amount) {
+    public User(String username, String email, String phone, String unit, String uniqueID, String groupID, String PIN, String address, String sex, String dob, long amount) {
         this.username = username;
         this.email = email;
         this.phone = phone;
@@ -15,6 +15,9 @@ public class User {
         this.uniqueID = uniqueID;
         GroupID = groupID;
         this.PIN = PIN;
+        this.address = address;
+        this.sex = sex;
+        this.dob = dob;
         this.amount = amount;
     }
 
@@ -72,6 +75,30 @@ public class User {
 
     public void setPIN(String PIN) {
         this.PIN = PIN;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
     }
 
     public long getAmount() {
