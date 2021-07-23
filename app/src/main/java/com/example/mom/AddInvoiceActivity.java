c998e2378efc31945e04deb2c40ca83a42b69778
DauMoe <lehuyhoang1111999@gmail.com>
2021-07-24 01:30:20 +0700

    (DOING) Modify MainActivity.java

--- app/src/main/java/com/example/mom/AddInvoiceActivity.java
+++ app/src/main/java/com/example/mom/AddInvoiceActivity.java
@@ -1,14 +1,41 @@
 package com.example.mom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.mom.databinding.ActivityAddInvoiceBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddInvoiceActivity extends AppCompatActivity {
+    private ActivityAddInvoiceBinding binding;
+    protected FirebaseFirestore db;
+    protected FirebaseAuth user;
+    List<String> list_cate = new ArrayList<>();
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_invoice);
+        binding     = DataBindingUtil.setContentView(this, R.layout.activity_add_invoice);
+        user        = FirebaseAuth.getInstance();
+        db          = FirebaseFirestore.getInstance();
+        list_cate.clear();
+        db.collection(DefineVars.CONSUMING_CATE).get().addOnSuccessListener(queryDocumentSnapshots -> {
+            for (QueryDocumentSnapshot i: queryDocumentSnapshots) {
+                list_cate.add(i.toString());
+            }
+            adapter = new ArrayAdapter(this, R.layout.list_item, list_cate);
+            binding.consumingCateItem.setAdapter(adapter);
+        });
     }
 }
